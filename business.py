f063e14aaba544d90daff0f9daa4009880e3ae4f
DESKTOP-CPRNSQO\matt <matt@mattbauman.com>
2019-01-01 22:42:05 -0600

    no message

--- business.py
+++ business.py
@@ -1,43 +1,98 @@
-import news
+import urllib.request
+import json
+import datetime
+import time
 import platform
-import os.path
-
-news_feed_urls = [
-    "https://www.npr.org/rss/rss.php?id=1006",
-    "http://feeds.bbci.co.uk/news/business/rss.xml",
-    "http://feeds.reuters.com/reuters/businessNews"]
-
-news_objects = []
+import os
 
 
-def get_news(url):
-    news_object = news.News(url)
-    news_object.get_rss()
-    news_object.parse_xml()
-    return news_object
+def get_json(url):
+    request_urlopen = urllib.request.urlopen(url)
+    response = request_urlopen.read().decode("utf-8")
+    return response
 
 
-for news_feed_url in news_feed_urls:
-    x = get_news(news_feed_url)
-    news_objects.append(x)
+def write_tr(write_date_time, write_weather_main, write_temp):
+    php_out.write(
+
+        "<tr>\n" +
+        "\t<td>" + write_date_time + "</td>\n" +
+        "\t<td>" + write_weather_main + "</td>\n" +
+        "\t<td>" + str(write_temp) + "\u00b0F</td>\n" +
+        "</tr>"
+    )
+
+
+def card_start(weekday, yyyy_mm_dd):
+    php_out.write('<div class="w3-card-4 w3-margin w3-white">' + 'Weather (Minneapolis)\n')
+    php_out.write('<div class ="w3-container">\n')
+    php_out.write('<h5><b>' + weekday + ', ' + yyyy_mm_dd + '</h5></b>\n')
+    php_out.write('<table>\n')
+
+
+def card_end():
+    php_out.write('</table>\n')
+    php_out.write('</div>\n')
+    php_out.write('</div>\n')
+
+
+def get_day_of_week(day_of_week_int):
+    if day_of_week_int == 0:
+        return "Monday"
+    elif day_of_week_int == 1:
+        return "Tuesday"
+    elif day_of_week_int == 2:
+        return "Wednesday"
+    elif day_of_week_int == 3:
+        return "Thursday"
+    elif day_of_week_int == 4:
+        return "Friday"
+    elif day_of_week_int == 5:
+        return "Saturday"
+    elif day_of_week_int == 6:
+        return "Sunday"
+
 
 if platform.system() == "Linux":
     path = "/opt/bitnami/apache2/htdocs/mattbauman.com/briefing/"
 else:
     path = "C:/Users/matt/Desktop"
 
-completeName = os.path.join(path, "business.php")
+completeName = os.path.join(path, "weather.php")
 php_out = open(completeName, "w")
+request_end_point = "http://api.openweathermap.org/data/2.5"
+request_api_name = "forecast"
+request_appid = "d298334de2c10c2ffb1781632245f212"
+request_city = "Minneapolis"
+request_country = "us"
+request_units = "imperial"
+# "http://api.openweathermap.org/data/2.5/forecast?q=Minneapolis,us&units=imperial&appid=d298334de2c10c2ffb1781632245f212"
+request = (request_end_point + "/" + request_api_name + "?q=" + request_city + "," + request_country + "&units="
+           + request_units + "&appid=" + request_appid)
+json_response = get_json(request)
+weather_json = json.loads(json_response)
+date_last = ""
+for weather_json_object in weather_json['list']:
+    date_time = weather_json_object['dt']
+    date_time_formatted = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(date_time))
+    date_str = str(date_time_formatted)[0:10]
+    time_str = str(date_time_formatted)[11:16]
+    time_str = datetime.datetime.strptime(time_str, '%H:%M').strftime('%I:%M %p')
+    temp_str = weather_json_object['main']['temp']  # main is a dict
+    temp = str(round(float(temp_str)))
+    weather_main = weather_json_object['weather'][0]['main']  # [weather] is a list, but take the first instance
+    # weather_description = weather_json_object['weather'][0]['description']
 
-for i in range(5):
-    for news_object in news_objects:
-        php_out.write("<div class=\"w3-card-4 w3-margin w3-white\">" + news_object.title + ' (' + str(i + 1) + ') \n')
-        php_out.write("\t<div class=\"w3-container\">\n")
-        php_out.write("\t\t<h5><b><a href=\""
-                      + news_object.reel_link[i]
-                      + "\" target=\"_blank\">"
-                      + news_object.reel_title[i]
-                      + "</a></b></h5>\n")
-        php_out.write("\t\t<p>" + news_object.reel_description[i] + "</p>\n")
-        php_out.write("\t</div>\n")
-        php_out.write("</div>\n")
+    date_weekday_int = datetime.date(int(date_str[0:4]), int(date_str[5:7]), int(date_str[8:10])).weekday()
+    date_weekday = get_day_of_week(date_weekday_int)
+
+    if date_last == "":  # first
+        card_start(date_weekday, date_str)
+    elif date_last != date_str:
+        card_end()
+        card_start(date_weekday, date_str)
+        write_tr(time_str, weather_main, temp)
+    else:
+        write_tr(time_str, weather_main, temp)
+    date_last = date_str
+card_end()
--- weather.py
+++ weather.py
@@ -1,6 +1,9 @@
 import urllib.request
 import json
+import datetime
 import time
+import platform
+import os
 
 
 def get_json(url):
@@ -9,32 +12,86 @@ def get_json(url):
     return response
 
 
+def write_tr(write_date_time, write_weather_main, write_temp):
+    print(
+
+        "          <tr>\r\n" +
+        "            <td>" + write_date_time + "</td>\r\n" +
+        "            <td>" + write_weather_main + "</td>\r\n" +
+        "            <td>" + str(write_temp) + "\u00b0F</td>\r\n" +
+        "          </tr>"
+    )
+
+
+def card_start(weekday, yyyy_mm_dd):
+    print('<div class="w3-card-4 w3-margin w3-white">' + 'Weather (Minneapolis)')
+    print('<div class ="w3-container">')
+    print('<h5><b>' + weekday + ', ' + yyyy_mm_dd + '</h5></b>')
+    print('<table>')
+
+
+def card_end(position):
+    print('</table>')
+    print('</div>')
+    print('</div>')
+
+
+def get_day_of_week(day_of_week_int):
+    if day_of_week_int == 0:
+        return "Monday"
+    elif day_of_week_int == 1:
+        return "Tuesday"
+    elif day_of_week_int == 2:
+        return "Wednesday"
+    elif day_of_week_int == 3:
+        return "Thursday"
+    elif day_of_week_int == 4:
+        return "Friday"
+    elif day_of_week_int == 5:
+        return "Saturday"
+    elif day_of_week_int == 6:
+        return "Sunday"
+
+
+if platform.system() == "Linux":
+    path = "/opt/bitnami/apache2/htdocs/mattbauman.com/briefing/"
+else:
+    path = "C:/Users/matt/Desktop"
+
+completeName = os.path.join(path, "weather.php")
+php_out = open(completeName, "w")
 request_end_point = "http://api.openweathermap.org/data/2.5"
 request_api_name = "forecast"
 request_appid = "d298334de2c10c2ffb1781632245f212"
 request_city = "Minneapolis"
 request_country = "us"
 request_units = "imperial"
-
 # "http://api.openweathermap.org/data/2.5/forecast?q=Minneapolis,us&units=imperial&appid=d298334de2c10c2ffb1781632245f212"
 request = (request_end_point + "/" + request_api_name + "?q=" + request_city + "," + request_country + "&units="
            + request_units + "&appid=" + request_appid)
-print(request)
 json_response = get_json(request)
-
 weather_json = json.loads(json_response)
-
+date_last = ""
 for weather_json_object in weather_json['list']:
     date_time = weather_json_object['dt']
     date_time_formatted = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(date_time))
-    temp = weather_json_object['main']['temp']  # main is a dict
+    date_str = str(date_time_formatted)[0:10]
+    time_str = str(date_time_formatted)[11:16]
+    time_str = datetime.datetime.strptime(time_str, '%H:%M').strftime('%I:%M %p')
+    temp_str = weather_json_object['main']['temp']  # main is a dict
+    temp = str(round(float(temp_str)))
     weather_main = weather_json_object['weather'][0]['main']  # [weather] is a list, but take the first instance
-    weather_description = weather_json_object['weather'][0]['description']
-    print(
+    # weather_description = weather_json_object['weather'][0]['description']
 
-        "          <tr>\r\n" +
-        "            <td>" + date_time_formatted + "</td>\r\n" +
-        "            <td>" + weather_main + ": " + weather_description + "</td>\r\n" +
-        "            <td>" + str(temp) + "\u00b0 F</td>\r\n" +
-        "          </tr>")
+    date_weekday_int = datetime.date(int(date_str[0:4]), int(date_str[5:7]), int(date_str[8:10])).weekday()
+    date_weekday = get_day_of_week(date_weekday_int)
 
+    if date_last == "":  # first
+        card_start(date_weekday, date_str)
+    elif date_last != date_str:
+        card_end()
+        card_start(date_weekday, date_str)
+        write_tr(time_str, weather_main, temp)
+    else:
+        write_tr(time_str, weather_main, temp)
+    date_last = date_str
