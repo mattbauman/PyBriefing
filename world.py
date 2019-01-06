3371330a6e3ffb77eca6047de8499e362cdd871a
mattbauman <matt@mattbauman.com>
2019-01-06 09:40:10 -0600

    Fixed BBC world news RSS URL

--- world.py
+++ world.py
@@ -2,7 +2,7 @@ import briefing
 
 file = "world.php"
 urls = [
-    "http://feeds.bbci.co.uk/news/video_and_audio/world/rss.xml",
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
     "http://feeds.reuters.com/Reuters/worldNews",
     "https://www.npr.org/rss/rss.php?id=1004",
     "https://www.pbs.org/newshour/feeds/rss/world"]
